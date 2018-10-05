2b9981a7d1548dffcbc9e42062894dfab0064c8b
zaffer <husse147@umn.edu>
2018-10-05 13:54:32 -0500

    final

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-/* CSci4061 F2018 Assignment 1
-* login: cselabs login name (login used to submit)
-* date: mm/dd/yy
-* name: full name1, full name2, full name3 (for partner(s))
-* id: id for first name, id for second name, id for third name */
+/* CSci4061 S2018 Assignment 1
+* login: husse147
+* date: 10/03/18
+* name: Christopher Pieper, Zaffer Hussein, John Hedlund-Fay (for partner(s))
+* id: 5160800, 5236353, 5181960 */
 
 // This is the main file for the code
 #include <errno.h>
